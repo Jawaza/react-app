32b8afdb82429f2846163a81a41da22a5f36450b
Jawaza <41973911+Jawaza@users.noreply.github.com>
2023-01-12 23:54:30 +1300

    Initial Setup
    Initial setup of the sample react application

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return ( 
+        <nav className="navigation">
+            <h1>Home</h1>
+            <div className="topmenu">
+                <a href="/">Profile</a>
+                <a href="/">Experience</a>
+                <a href="/">Skills</a>
+                <a href="/">Contact</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
